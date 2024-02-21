1d388ae3d08fd6acd204753db4ac8f96fcfc8d6a
Hayzed-codes <raheemhorlah@gmail.com>
2024-02-21 12:42:26 +0100

    more

--- arrayInDetails/script.js
+++ arrayInDetails/script.js
@@ -7,8 +7,21 @@ const cohorts = [
   "Oloye Pupo",
   "Abdul-Lateef",
   "Mr Tunmise",
+  
 ];
 console.log(cohorts);
+
+const replaceEl = (cohorts[1] = 'Bash')
+
+// ES5
+for(let i = 0; i < cohorts.length; i++) {
+    console.log(i, cohorts[i])
+}
+
+// ES6
+for(const list of cohorts) {
+    console.log(list)
+};
 
 const nested = [[1,2], [3,4], [5,6]]
 const newArr = nested[0][1]
@@ -29,6 +42,10 @@ const arr = [
         age : 80
     },
 
+    function name (){
+        alert('DEAD MEAT 🔪')
+    }
 
 ]
-console.log(arr)
+console.log(typeof arr);
+
