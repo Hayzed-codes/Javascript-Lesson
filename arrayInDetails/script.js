2ddeff7696858a8d5c6516a9c6460aeedcc0e1ed
Hayzed-codes <raheemhorlah@gmail.com>
2024-02-22 12:04:44 +0100

    update

--- arrayInDetails/script.js
+++ arrayInDetails/script.js
@@ -73,6 +73,7 @@ const rmFirst = colors.shift();
 console.log(rmFirst);
 console.log(colors)
 
+// UNSHIFT()
 const month = ['March', 'July', 'September', 'January'];
 const adMonth = month.unshift('February');
 console.log(adMonth);
@@ -81,16 +82,69 @@ console.log(month)
 const countries = ['Kenya', 'Malaysia', 'Canada', 'Qatar', 'Nigeria', 'Morro', 'Southy'];
 console.log(countries)
 
+// SPLICE()
 countries.splice(3, 2, 'Egypt', 'Ghana');
 console.log(countries)
 
 const wears = ['Gucci', 'Versace', 'D&G', 'Nike', 'LV'];
 console.log(wears)
 
-
+wears.splice(2)
+console.log(wears)
 
 const drink = ['coke', 'viju', 'Gin', 'Exotic', 'Bobo']
 console.log(drink)
 
 drink.splice(2, 1)
 console.log(drink)
+
+//  SLICE ()
+
+const men = ['Tunmise', 'Kenny', 'Soliu', 'Bode', 'Abee', 'Hayzed'];
+console.log(men)
+
+const baddestMen = men.slice(2)
+console.log(baddestMen)
+
+const newString = 'Nafeesah'
+console.log(newString.length);
+const newSlice = newString.slice(2, 7);
+console.log(newSlice)
+
+const name = 'Azeez'
+console.log(name)
+
+const midName = name.slice(1, 4)
+console.log(midName)
+
+const coolestAmong = men.slice(5)
+console.log(coolestAmong)
+
+// toString() 
+
+const menString = men.toString();
+console.log(menString)
+
+// .include()
+
+const include = men.includes('Seyi');
+console.log(include)
+
+// sort()
+
+men.sort();
+console.log(men);
+
+const alpha = ['G', 'F', 'K', 'L', 'A', 'P'];
+console.log(alpha)
+
+alpha.sort();
+console.log(alpha)
+
+const num = [5, 8, 0, 5, 7, 2, 1, 9];
+console.log(num)
+
+num.sort();
+console.log(num)
+
+// CALL BACK FUNCTION () => {}
