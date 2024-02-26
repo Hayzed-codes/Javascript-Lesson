3771f6dd7af23917ad3f28f93f6eeb588293b108
Hayzed-codes <raheemhorlah@gmail.com>
2024-02-26 15:28:58 +0100

    assessAndAdding

---
+++ objectAndItsMethods/script.js
@@ -0,0 +1,35 @@
+// OBJECTS
+
+// const firstName = "Soliu"
+// const person = {
+//     firstName,
+//     surname: "Hayzed",
+//     age: 158,
+//     children: {
+//         firstChild: "Eef",
+//         secondChild: "Nafisah",
+
+//     }
+// }
+
+// console.log(person)
+
+
+// ASSESSING AND ADDING OF PROPERTIES TO AN OBJECT
+
+const human = {
+    firstName: 'Abdullah',
+    "DLT africa location": "Pegamut" 
+}
+
+human.pet = {name:"Cobra", age:3}
+human.job = "Headmaster"
+human.favFood = "Garri"
+
+console.log(human)
+
+
+console.log(human["DLT africa location"])
+console.log(human['firstName'])
+console.log(human.firstName)
+console.log(human.pet.name)
